5884f658786bf8e4049f69fe11b2873e7e67fb15
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-09-30 10:15:23 -0500

    detector.py

--- detector.py
+++ detector.py
@@ -1,29 +1,12 @@
-# Instala la herramienta de etiquetado:
-#install labelme
-
-# Si te pide actualizar pip:
-#python.exe -m pip install --upgrade pip
-
-# Ejecuta labelme:
-#labelme
-
-# Installa labelme2yolo para convertir Json a Yolo:
-#pip install labelme2yolo
-
-# Convierte tus archivos Json a Yolo:
-#labelme2yolo --json_dir tu/direccion/sin/espacios
-
 # Instala ultralytics:
 #install ultralytics
 
-# Verifica tu tarjeta grafica:
+# Verifica que se use tu tarjeta grafica:
 #python
 #import torch
 #torch.cuda.is_available()
 
 # Entrena tu red neuronal:
-#yolo task=segment mode=train epochs=30 data=C:\Users\ASUS\Desktop\Detector\custom.yaml model=yolov8m-seg.pt imgsz=640 batch=2
-
 
 from ultralytics import YOLO
 import torch
